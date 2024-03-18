5c098f884de5097e54b5a0a1aead01c39096cd6f
navnathdeshmukh45 <navnathdeshmukh363@gmail.com>
2024-03-18 19:46:11 +0530

    initial commit

---
+++ Day51/script.js
@@ -0,0 +1,17 @@
+alert("Day 51")
+const loginForm = document.getElementById('login-form');
+const errorMessage = document.getElementById('error-message');
+
+loginForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    if (username === 'admin' && password === 'Pass@123') {
+        // Successful login, you can redirect or perform other actions here
+        alert('Login successful');
+    } else {
+        errorMessage.textContent = 'Invalid username or password';
+    }
+});
